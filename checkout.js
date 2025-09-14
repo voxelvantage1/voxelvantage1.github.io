609721bfd0f897c25745667b7f2846e31d62b1a1
voxelvantage1 <accounts@voxelvantage.com>
2025-09-14 15:27:08 -0500

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -1,18 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <title>Accept a payment</title>
-    <meta name="description" content="A demo of a payment on Stripe" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" href="style.css" />
-    <script src="https://js.stripe.com/basil/stripe.js"></script>
-    <script src="checkout.js" defer></script>
-  </head>
-  <body>
-    <!-- Display a payment form -->
-      <div id="checkout">
-        <!-- Checkout will insert the payment form here -->
-      </div>
-  </body>
-</html>
+// This is your test publishable API key.
+const stripe = Stripe("pk_test_51RmTqr2eR7Ar9niv9K5AAaL7ez73zIil5lZKArHe4rmC1v2mbo5na9rChLHeXC6m8ftg4nFJ13HBIVrPSzKBqdo500AXQuH7IC");
+
+initialize();
+
+// Create a Checkout Session
+async function initialize() {
+  const fetchClientSecret = async () => {
+    const response = await fetch("http://108.253.196.110:4242/create-checkout-session", {
+      method: "POST",
+    });
+    const { clientSecret } = await response.json();
+    return clientSecret;
+  };
+
+  const checkout = await stripe.initEmbeddedCheckout({
+    fetchClientSecret,
+  });
+
+  // Mount Checkout
+  checkout.mount('#checkout');
+}
